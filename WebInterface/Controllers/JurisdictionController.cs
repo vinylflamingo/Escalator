a1d9675ce59bc799a770e4695269ea2e5e40f185
vinylflamingo <55264795+franciscocostoyacentralsquare@users.noreply.github.com>
2020-12-16 20:14:20 -0500

    Jurisdiction Add Form

--- WebInterface/Controllers/JurisdictionController.cs
+++ WebInterface/Controllers/JurisdictionController.cs
@@ -1,7 +1,45 @@
+using System.Threading.Tasks;
+using Escalator.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using WebInterface.Models;
+using WebInterface.Processors;
+
 namespace WebInterface.Controllers
 {
-    public class JurisdictionController
+    public class JurisdictionController : Controller
     {
+        private JurisdictionProcessor _jurisdictionProcessor;
+
+        public JurisdictionController(JurisdictionProcessor jurisdictionProcessor)
+        {
+            _jurisdictionProcessor = jurisdictionProcessor;
+
+        }
         
+        public async Task<IActionResult> Index()
+        {
+
+            JurisdictionsViewModel model = new JurisdictionsViewModel()
+            {
+                jurisdictions = await _jurisdictionProcessor.LoadJurisdictions()
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult New()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> New(Jurisdiction jurisdiction)
+        {       
+            var result = await _jurisdictionProcessor.SaveJurisdiction(jurisdiction);
+            return RedirectToAction("Index");
+
+        }
     }
 }
---
+++ WebInterface/Models/JurisdictionsViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Escalator.Common.Models;
+
+namespace WebInterface.Models
+{
+    public class JurisdictionsViewModel
+    {
+        public IEnumerable<Jurisdiction> jurisdictions {get; set;}
+    }
+}
